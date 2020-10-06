c5fdb9d4b553cfac54a62da8b089a9b687d499b6
Gus Fowler <gus.fowler12@gmail.com>
2020-10-05 19:55:35 -0700

    initial version

---
+++ AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  // @param value adds value to the total
+  public void add (int value) {
+  }
+  // @param value subtracts from the total
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
