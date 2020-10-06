3df9676e5908b09a6042e7a5e645a96b8d53602a
Gus Fowler <gus.fowler12@gmail.com>
2020-10-05 20:06:31 -0700

    second version

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,33 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String memory;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    memory = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   // @param value adds value to the total
   public void add (int value) {
+      total += value;
+      memory = memory + " + " + String.parseInt(value);
   }
   // @param value subtracts from the total
   public void subtract (int value) {
+      total -= value;
+      memory = memory + " - " + String.parseInt(value);
   }
 
   public String toString () {
-    return "";
+    //memory = memory + " = " + String.parseInt(total);
+    return memory;
   }
 
   public void clear() {
+      memory = "0";
   }
 }
